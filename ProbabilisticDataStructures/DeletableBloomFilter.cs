8ecf5819322c376e084ed98f5b70ad5c937e5b4e
mattlorimor <mattlorimor@gmail.com>
2015-12-08 20:15:34 -0600

    Fully implement the DeletableBloomFilter.

---
+++ ProbabilisticDataStructures/DeletableBloomFilter.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbabilisticDataStructures
+{
+    /// <summary>
+    /// DeletableBloomFilter implements a Deletable Bloom Filter as described by
+    /// Rothenberg, Macapuna, Verdi, Magalhaes in The Deletable Bloom filter - A new
+    /// member of the Bloom family:
+    ///
+    /// http://arxiv.org/pdf/1005.0352.pdf
+    ///
+    /// A Deletable Bloom Filter compactly stores information on collisions when
+    /// inserting elements. This information is used to determine if elements are
+    /// deletable. This design enables false-negative-free deletions at a fraction
+    /// of the cost in memory consumption.
+    ///
+    /// Deletable Bloom Filters are useful for cases which require removing elements
+    /// but cannot allow false negatives. This means they can be safely swapped in
+    /// place of traditional Bloom filters.
+    /// </summary>
+    public class DeletableBloomFilter : IFilter
+    {
+        /// <summary>
+        /// Filter data
+        /// </summary>
+        public Buckets Buckets { get; private set; }
+        /// <summary>
+        /// Filter collision data
+        /// </summary>
+        public Buckets Collisions { get; private set; }
+        /// <summary>
+        /// Hash algorithm
+        /// </summary>
+        private HashAlgorithm hash { get; set; }
+        /// <summary>
+        /// Filter size
+        /// </summary>
+        private uint m { get; set; }
+        /// <summary>
+        /// Number of bits in a region
+        /// </summary>
+        private uint regionSize { get; set; }
+        /// <summary>
+        /// Number of hash functions
+        /// </summary>
+        private uint k { get; set; }
+        /// <summary>
+        /// Number of items in the filter
+        /// </summary>
+        private uint count { get; set; }
+        /// <summary>
+        /// Buffer used to cache indices
+        /// </summary>
+        private uint[] indexBuffer { get; set; }
+
+        /// <summary>
+        /// NewDeletableBloomFilter creates a new DeletableBloomFilter optimized to store
+        /// n items with a specified target false-positive rate. The r value determines
+        /// the number of bits to use to store collision information. This controls the
+        /// deletability of an element. Refer to the paper for selecting an optimal value.
+        /// </summary>
+        /// <param name="n">Number of items</param>
+        /// <param name="r">Number of bits to use to store collision information</param>
+        /// <param name="fpRate">Desired false positive rate</param>
+        public DeletableBloomFilter(uint n, uint r, double fpRate)
+        {
+            var m = ProbabilisticDataStructures.OptimalM(n, fpRate);
+            var k = ProbabilisticDataStructures.OptimalK(fpRate);
+
+            this.Buckets = new Buckets(m - r, 1);
+            this.Collisions = new Buckets(r, 1);
+            this.hash = HashAlgorithm.Create("MD5");
+            this.m = m - r;
+            this.regionSize = (m - r) / r;
+            this.k = k;
+            this.indexBuffer = new uint[k];
+        }
+
+        /// <summary>
+        /// Returns the Bloom filter capacity, m.
+        /// </summary>
+        /// <returns>The Bloom filter capacity, m</returns>
+        public uint Capacity()
+        {
+            return this.m;
+        }
+
+        /// <summary>
+        /// Returns the number of hash functions.
+        /// </summary>
+        /// <returns>The number of hash functions</returns>
+        public uint K()
+        {
+            return this.k;
+        }
+
+        /// <summary>
+        /// Returns the number of items added to the filter.
+        /// </summary>
+        /// <returns>The number of items added to the filter</returns>
+        public uint Count()
+        {
+            return this.count;
+        }
+
+        /// <summary>
+        /// Will test for membership of the data and returns true if it is a member,
+        /// false if not. This is a probabilistic test, meaning there is a non-zero
+        /// probability of false positives but a zero probability of false negatives.
+        /// </summary>
+        /// <param name="data">The data to search for.</param>
+        /// <returns>Whether or not the data is maybe contained in the filter.</returns>
+        public bool Test(byte[] data)
+        {
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+
+            // If any of the K bits are not set, then it's not a member.
+            for (uint i = 0; i < this.k; i++)
+            {
+                if (this.Buckets.Get((lower + upper * i) % this.m) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Will add the data to the Bloom filter. It returns the filter to allow
+        /// for chaining.
+        /// </summary>
+        /// <param name="data">The data to add.</param>
+        /// <returns>The filter.</returns>
+        public IFilter Add(byte[] data)
+        {
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+
+            // Set the K bits.
+            for (uint i = 0; i < this.k; i++)
+            {
+                var idx = (lower + upper * i) % this.m;
+                if (this.Buckets.Get(idx) != 0)
+                {
+                    // Collision, set corresponding region bit.
+                    this.Collisions.Set(idx / this.regionSize, 1);
+                }
+                else
+                {
+                    this.Buckets.Set(idx, 1);
+                }
+            }
+
+            this.count++;
+            return this;
+        }
+
+        /// <summary>
+        /// Is equivalent to calling Test followed by Add. It returns true if the data is
+        /// a member, false if not.
+        /// </summary>
+        /// <param name="data">The data to test for and add if it doesn't exist.</param>
+        /// <returns>Whether or not the data was probably contained in the filter.</returns>
+        public bool TestAndAdd(byte[] data)
+        {
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+            var member = true;
+
+            // If any of the K bits are not set, then it's not a member.
+            for (uint i = 0; i < this.k; i++)
+            {
+                var idx = (lower + upper * i) % this.m;
+                if (this.Buckets.Get(idx) == 0)
+                {
+                    member = false;
+                }
+                else
+                {
+                    // Collision, set corresponding region bit.
+                    this.Collisions.Set(idx / this.regionSize, 1);
+                }
+                this.Buckets.Set(idx, 1);
+            }
+
+            this.count++;
+            return member;
+        }
+
+        /// <summary>
+        /// Will test for membership of the data and remove it from the filter if it
+        /// exists. Returns true if the data was a member, false if not.
+        /// </summary>
+        /// <param name="data">The data to test for and remove</param>
+        /// <returns>Whether or not the data was a member before this call</returns>
+        public bool TestAndRemove(byte[] data)
+        {
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+            var member = true;
+
+            // Set the K bits.
+            for (uint i = 0; i < this.k; i++)
+            {
+                var idx = (lower + upper * i) % this.m;
+                this.indexBuffer[i] = idx;
+                if (this.Buckets.Get(idx) == 0)
+                {
+                    member = false;
+                }
+            }
+
+            if (member)
+            {
+                foreach (var idx in this.indexBuffer)
+                {
+                    if (this.Collisions.Get(idx / this.regionSize) == 0)
+                    {
+                        // Clear only bits located in collision-free zones.
+                        this.Buckets.Set(idx, 0);
+                    }
+                }
+                this.count--;
+            }
+
+            return member;
+        }
+
+        /// <summary>
+        /// Restores the Bloom filter to its original state. It returns the filter to
+        /// allow for chaining.
+        /// </summary>
+        /// <returns>The reset bloom filter.</returns>
+        public DeletableBloomFilter Reset()
+        {
+            this.Buckets.Reset();
+            this.Collisions.Reset();
+            this.count = 0;
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the hashing function used in the filter.
+        /// </summary>
+        /// <param name="h">The HashAlgorithm to use.</param>
+        // TODO: Add SetHash to the IFilter interface?
+        public void SetHash(HashAlgorithm h)
+        {
+            this.hash = h;
+        }
+    }
+}
---
+++ TestProbabilisticDataStructures/TestDeletableBloomFilter.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProbabilisticDataStructures;
+using System.Text;
+
+namespace TestProbabilisticDataStructures
+{
+    [TestClass]
+    public class TestDeletableBloomFilter
+    {
+        private static byte[] A_BYTES = Encoding.ASCII.GetBytes("a");
+        private static byte[] B_BYTES = Encoding.ASCII.GetBytes("b");
+        private static byte[] C_BYTES = Encoding.ASCII.GetBytes("c");
+        private static byte[] X_BYTES = Encoding.ASCII.GetBytes("x");
+
+        /// <summary>
+        /// Ensures that Capacity() returns the number of bits, m, in the Bloom filter.
+        /// </summary>
+        [TestMethod]
+        public void TestDeletableCapacity()
+        {
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+            var capacity = d.Capacity();
+
+            Assert.AreEqual(470u, capacity);
+        }
+
+        /// <summary>
+        /// Ensures that K() returns the number of hash functions in the Bloom Filter.
+        /// </summary>
+        [TestMethod]
+        public void TestDeletableK()
+        {
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+            var k = d.K();
+
+            Assert.AreEqual(4u, k);
+        }
+
+        /// <summary>
+        /// Ensures that Count returns the number of items added to the filter.
+        /// </summary>
+        [TestMethod]
+        public void TestDeletableCount()
+        {
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+            for (uint i = 0; i < 10; i++)
+            {
+                d.Add(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                d.TestAndRemove(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            var count = d.Count();
+            Assert.AreEqual(5u, count);
+        }
+
+        /// <summary>
+        /// Ensures that Test, Add, and TestAndAdd behave correctly.
+        /// </summary>
+        [TestMethod]
+        public void TestDeletableTestAndAdd()
+        {
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+
+            // 'a' is not in the filter.
+            if (d.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should not be a member");
+            }
+
+            var addedF = d.Add(A_BYTES);
+            Assert.AreSame(d, addedF, "Returned CountingBloomFilter should be the same instance");
+
+            // 'a' is now in the filter.
+            if (!d.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'a' is still in the filter.
+            if (!d.TestAndAdd(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'b' is not in the filter.
+            if (d.TestAndAdd(B_BYTES))
+            {
+                Assert.Fail("'b' should not be a member");
+            }
+
+            // 'a' is still in the filter.
+            if (!d.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'b' is now in the filter.
+            if (!d.Test(B_BYTES))
+            {
+                Assert.Fail("'b' should be a member");
+            }
+
+            // 'c' is not in the filter.
+            if (d.Test(C_BYTES))
+            {
+                Assert.Fail("'c' should not be a member");
+            }
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                d.TestAndAdd(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            // 'x' should be a false positive.
+            if (!d.Test(X_BYTES))
+            {
+                Assert.Fail("'x' should be a member");
+            }
+        }
+
+        /// <summary>
+        /// Ensures that TestAndRemove behaves correctly.
+        /// </summary>
+        [TestMethod]
+        public void TestDeletableTestAndRemove()
+        {
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+
+            // 'a' is not in the filter.
+            if (d.TestAndRemove(A_BYTES))
+            {
+                Assert.Fail("'a' should not be a member");
+            }
+
+            d.Add(A_BYTES);
+
+            // 'a' is now in the filter.
+            if (!d.TestAndRemove(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'a' is no longer in the filter.
+            if (d.TestAndRemove(A_BYTES))
+            {
+                Assert.Fail("'a' should not be a member");
+            }
+        }
+
+        /// <summary>
+        /// Ensures that Reset sets every bit to zero.
+        /// </summary>
+        [TestMethod]
+        public void TestDeletableReset()
+        {
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+            for (int i = 0; i < 1000; i++)
+            {
+                d.Add(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            var resetF = d.Reset();
+            Assert.AreSame(d, resetF, "Returned DeletableBloomFilter should be the same instance");
+
+            for (uint i = 0; i < d.Buckets.Count; i++)
+            {
+                if (d.Buckets.Get(i) != 0)
+                {
+                    Assert.Fail("Expected all bits to be unset");
+                }
+            }
+
+            for (uint i = 0; i < d.Collisions.Count; i++)
+            {
+                if (d.Collisions.Get(i) != 0)
+                {
+                    Assert.Fail("Expected all bits to be unset");
+                }
+            }
+
+            var count = d.Count();
+            Assert.AreEqual(0u, count);
+        }
+
+        [TestMethod]
+        public void BenchmarkDeletableAdd()
+        {
+            var n = 100000;
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+            var data = new byte[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = Encoding.ASCII.GetBytes(i.ToString());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                d.Add(data[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkDeletableTest()
+        {
+            var n = 100000;
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+            var data = new byte[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = Encoding.ASCII.GetBytes(i.ToString());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                d.Test(data[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkDeletableTestAndAdd()
+        {
+            var n = 100000;
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+            var data = new byte[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = Encoding.ASCII.GetBytes(i.ToString());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                d.TestAndAdd(data[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkDeletableTestAndRemove()
+        {
+            var n = 100000;
+            var d = new DeletableBloomFilter(100, 10, 0.1);
+            var data = new byte[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = Encoding.ASCII.GetBytes(i.ToString());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                d.TestAndRemove(data[i]);
+            }
+        }
+    }
+}
