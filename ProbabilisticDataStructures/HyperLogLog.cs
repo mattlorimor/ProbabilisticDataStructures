f1a13ad3b1231d4772a166a0711e8a968d5472eb
mattlorimor <mattlorimor@gmail.com>
2015-12-08 23:13:34 -0600

    Implement HyperLogLog.

---
+++ ProbabilisticDataStructures/HyperLogLog.cs
@@ -0,0 +1,262 @@
+﻿/*
+Original work Copyright 2013 Eric Lesh
+Modified work Copyright 2015 Tyler Treat
+Modified work Copyright 2015 Matthew Lorimor
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbabilisticDataStructures
+{
+    /// <summary>
+    /// implements the HyperLogLog cardinality estimation algorithm as
+    /// described by Flajolet, Fusy, Gandouet, and Meunier in HyperLogLog: the
+    /// analysis of a near-optimal cardinality estimation algorithm:
+    ///
+    /// http://algo.inria.fr/flajolet/Publications/FlFuGaMe07.pdf
+    ///
+    /// HyperLogLog is a probabilistic algorithm which approximates the number of
+    /// distinct elements in a multiset. It works by hashing values and calculating
+    /// the maximum number of leading zeros in the binary representation of each
+    /// hash. If the maximum number of leading zeros is n, the estimated number of
+    /// distinct elements in the set is 2^n. To minimize variance, the multiset is
+    /// split into a configurable number of registers, the maximum number of leading
+    /// zeros is calculated in the numbers in each register, and a harmonic mean is
+    /// used to combine the estimates.
+    ///
+    /// For large or unbounded data sets, calculating the exact cardinality is
+    /// impractical. HyperLogLog uses a fraction of the memory while providing an
+    /// accurate approximation. For counting element frequency, refer to the
+    /// Count-Min Sketch.
+    /// </summary>
+    public class HyperLogLog
+    {
+        private static double exp32 = Math.Pow(2, 32);
+
+        /// <summary>
+        /// Counter registers
+        /// </summary>
+        private byte[] registers { get; set; }
+        /// <summary>
+        /// Number of registers
+        /// </summary>
+        public uint m { get; private set; }
+        /// <summary>
+        /// Number of bits to calculate register
+        /// </summary>
+        private uint b { get; set; }
+        /// <summary>
+        /// Bias-correction constant
+        /// </summary>
+        private double alpha { get; set; }
+        /// <summary>
+        /// Hash algorithm
+        /// </summary>
+        private HashAlgorithm hash { get; set; }
+
+        /// <summary>
+        /// Creates a new HyperLogLog with m registers. Returns an error if m isn't a
+        /// power of two.
+        /// </summary>
+        /// <param name="m">Number of registers (must be a power of two)</param>
+        public HyperLogLog(uint m)
+        {
+            if ((m & (m - 1)) != 0)
+            {
+                throw new ArgumentException(String.Format("{0} is not a power of two"));
+            }
+
+            this.registers = new byte[m];
+            this.m = m;
+            this.b = (uint)Math.Ceiling(Math.Log(m, 2));
+            this.alpha = this.calculateAlpha(m);
+            this.hash = HashAlgorithm.Create("MD5");
+        }
+
+        /// <summary>
+        /// Creates a new HyperLogLog optimized for the specified standard error.
+        /// Throws an ArgumentException if the number of registers can't be calculated
+        /// for the provided accuracy.
+        /// </summary>
+        /// <param name="e">Desired standard error</param>
+        /// <returns>The HyperLogLog optimized for the standard error</returns>
+        public static HyperLogLog NewDefaultHyperLogLog(double e)
+        {
+            var m = Math.Pow(1.04 / e, 2);
+            return new HyperLogLog((uint)Math.Pow(2, Math.Ceiling(Math.Log(m, 2))));
+        }
+
+        /// <summary>
+        /// Will add the data to the set. Returns the HyperLogLog to allow for chaining.
+        /// </summary>
+        /// <param name="data">The data to add</param>
+        /// <returns>The HyperLogLog</returns>
+        public HyperLogLog Add(byte[] data)
+        {
+            var hash = calculateHash(data);
+            var k = 32 - this.b;
+            var r = this.calculateRho(hash << (int)this.b, k);
+            var j = hash >> (int)k;
+
+            if (r > this.registers[j])
+            {
+                this.registers[j] = r;
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Returns the approximated cardinality of the set.
+        /// </summary>
+        /// <returns>The approximated cardinality of the set</returns>
+        public UInt64 Count()
+        {
+            var sum = 0.0;
+            var m = (double)this.m;
+            foreach (var val in this.registers)
+            {
+                sum += 1.0 / Math.Pow(2.0, val);
+            }
+            var estimate = this.alpha * m * m / sum;
+            if (estimate <= 5.0 / 2.0 * m)
+            {
+                // Small range correction
+                var v = 0;
+                foreach (var r in this.registers)
+                {
+                    if (r == 0)
+                    {
+                        v++;
+                    }
+                }
+                if (v > 0)
+                {
+                    estimate = m * Math.Log(m / v);
+                }
+            }
+            else if (estimate > 1.0 / 30.0 * exp32)
+            {
+                // Large range correction
+                estimate = -exp32 * Math.Log(1 - estimate / exp32);
+            }
+            return (UInt64)estimate;
+        }
+
+        /// <summary>
+        /// Combines this HyperLogLog with another. Returns an error if the number of
+        /// registers in the two HyperLogLogs are not equal.
+        /// </summary>
+        /// <param name="other">The HyperLogLog to merge</param>
+        /// <returns>Whether or not the merge was successful</returns>
+        public bool Merge(HyperLogLog other)
+        {
+            if (this.m != other.m)
+            {
+                throw new ArgumentException("Number of registers must match");
+            }
+
+            for (int i = 0; i < other.registers.Count(); i++)
+            {
+                var r = other.registers[i];
+                if (r > this.registers[i])
+                {
+                    this.registers[i] = r;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Restores the HyperLogLog to its original state. It returns itself to allow
+        /// for chaining.
+        /// </summary>
+        /// <returns>The HyperLogLog</returns>
+        public HyperLogLog Reset()
+        {
+            this.registers = new byte[this.m];
+            return this;
+        }
+
+        /// <summary>
+        /// Returns a 32-bit hash value for the given data.
+        /// </summary>
+        /// <param name="data">Data</param>
+        /// <returns>32-bit hash value</returns>
+        private uint calculateHash(byte[] data)
+        {
+            var hash = new Hash(this.hash);
+            hash.ComputeHash(data);
+            var sum = hash.Sum();
+            return ProbabilisticDataStructures.ToBigEndianUInt32(sum);
+        }
+
+        /// <summary>
+        /// Sets the hashing function used in the filter.
+        /// </summary>
+        /// <param name="h">The HashAlgorithm to use.</param>
+        public void SetHash(HashAlgorithm h)
+        {
+            this.hash = h;
+        }
+
+        /// <summary>
+        /// Calculates the bias-correction constant alpha based on the number of
+        /// registers, m.
+        /// </summary>
+        /// <param name="m">Number of registers</param>
+        /// <returns>Calculated bias-correction constant, alpha</returns>
+        private double calculateAlpha(uint m)
+        {
+            switch (m)
+            {
+                case 16:
+                    return 0.673;
+                case 32:
+                    return 0.697;
+                case 64:
+                    return 0.709;
+                default:
+                    return 0.7213 / (1.0 + 1.079 / m);
+            }
+        }
+
+        /// <summary>
+        /// Calculates the position of the leftmost 1-bit.
+        /// </summary>
+        /// <param name="val">The value to check</param>
+        /// <param name="max"></param>
+        /// <returns>The position of the leftmost 1-bit</returns>
+        private byte calculateRho(uint val, uint max)
+        {
+            var r = 1;
+            while ((val & 0x80000000) == 0 && r <= max)
+            {
+                r++;
+                val <<= 1;
+            }
+            return (byte)r;
+        }
+
+        // TODO: Implement these later.
+        // WriteDataTo
+        // ReadDataFrom
+    }
+}
---
+++ TestProbabilisticDataStructures/TestHyperLogLog.cs
@@ -0,0 +1,149 @@
+﻿/*
+Original work Copyright 2013 Eric Lesh
+Modified work Copyright 2015 Tyler Treat
+Modified work Copyright 2015 Matthew Lorimor
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+*/
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProbabilisticDataStructures;
+using System.Text;
+
+namespace TestProbabilisticDataStructures
+{
+    [TestClass]
+    public class TestHyperLogLog
+    {
+        private double geterror(UInt64 actual, UInt64 estimate)
+        {
+            return ((float)estimate - (float)actual) / (float)actual;
+        }
+
+        private void testHyperLogLog(int n, int lowB, int highB)
+        {
+            var words = Words.Dictionary(n);
+            var bad = 0;
+            var nWords = (UInt64)words.LongLength;
+            for (int i = lowB; i < highB; i++)
+            {
+                var m = (uint)Math.Pow(2, i);
+
+                HyperLogLog h = null;
+                try
+                {
+                    h = new HyperLogLog(m);
+                }
+                catch (Exception)
+                {
+                    Assert.Fail(string.Format("Can't make HyperLogLog({0})", m));
+                }
+
+                foreach (var word in words)
+                {
+                    h.Add(Encoding.ASCII.GetBytes(word));
+                }
+
+                var expectedError = 1.04 / Math.Sqrt(m);
+                var actualError = Math.Abs(this.geterror(nWords, h.Count()));
+
+                if (actualError > expectedError)
+                {
+                    bad++;
+                    //Assert.Fail(string.Format("Expected: {0}, Actual: {1}", expectedError, actualError));
+                }
+            }
+        }
+
+        private void benchmarkCount(int registers)
+        {
+            var n = 100000;
+            var words = Words.Dictionary(0);
+            var m = (uint)Math.Pow(2, registers);
+
+            var h = new HyperLogLog(m);
+
+            foreach (var word in words)
+            {
+                h.Add(Encoding.ASCII.GetBytes(word));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                h.Count();
+            }
+        }
+
+        [TestMethod]
+        public void TestHyperLogLogSmall()
+        {
+            this.testHyperLogLog(5, 4, 17);
+        }
+
+        [TestMethod]
+        public void TestHyperLogLogBig()
+        {
+            this.testHyperLogLog(0, 4, 17);
+        }
+
+        [TestMethod]
+        public void TestNewDefaultHyperLogLog()
+        {
+            var hll = HyperLogLog.NewDefaultHyperLogLog(0.1);
+
+            Assert.AreEqual(128u, hll.m);
+        }
+
+        [TestMethod]
+        public void BenchmarkHLLCount4()
+        {
+            this.benchmarkCount(4);
+        }
+
+        [TestMethod]
+        public void BenchmarkHLLCount5()
+        {
+            this.benchmarkCount(5);
+        }
+
+        [TestMethod]
+        public void BenchmarkHLLCount6()
+        {
+            this.benchmarkCount(6);
+        }
+
+        [TestMethod]
+        public void BenchmarkHLLCount7()
+        {
+            this.benchmarkCount(7);
+        }
+
+        [TestMethod]
+        public void BenchmarkHLLCount8()
+        {
+            this.benchmarkCount(8);
+        }
+
+        [TestMethod]
+        public void BenchmarkHLLCount9()
+        {
+            this.benchmarkCount(9);
+        }
+
+        [TestMethod]
+        public void BenchmarkHLLCount10()
+        {
+            this.benchmarkCount(10);
+        }
+    }
+}
