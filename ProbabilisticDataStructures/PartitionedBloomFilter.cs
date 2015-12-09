85b62a3297cdde5abbc11e0272e7da2715d94a62
mattlorimor <mattlorimor@gmail.com>
2015-12-09 02:20:05 -0600

    Implement PartitionedBloomFilter

---
+++ ProbabilisticDataStructures/PartitionedBloomFilter.cs
@@ -0,0 +1,248 @@
+﻿/*
+Original work Copyright (c) 2013 zhenjl
+Modified work Copyright (c) 2015 Tyler Treat
+Modified work Copyright (c) 2015 Matthew Lorimor
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbabilisticDataStructures
+{
+    /// <summary>
+    /// PartitionedBloomFilter implements a variation of a classic Bloom filter as
+    /// described by Almeida, Baquero, Preguica, and Hutchison in Scalable Bloom
+    /// Filters:
+    ///
+    /// http://gsd.di.uminho.pt/members/cbm/ps/dbloom.pdf
+    ///
+    /// This filter works by partitioning the M-sized bit array into k slices of
+    /// size m = M/k bits. Each hash function produces an index over m for its
+    /// respective slice. Thus, each element is described by exactly k bits, meaning
+    /// the distribution of false positives is uniform across all elements.
+    /// </summary>
+    public class PartitionedBloomFilter : IFilter
+    {
+        /// <summary>
+        /// Partitioned filter data
+        /// </summary>
+        public Buckets[] Partitions { get; private set; }
+        /// <summary>
+        /// Hash algorithm
+        /// </summary>
+        private HashAlgorithm hash { get; set; }
+        /// <summary>
+        /// Filter size (divided into k partitions)
+        /// </summary>
+        private uint m { get; set; }
+        /// <summary>
+        /// Number of hash functions (and partitions)
+        /// </summary>
+        private uint k { get; set; }
+        /// <summary>
+        /// Partition size (m / k)
+        /// </summary>
+        private uint s { get; set; }
+        /// <summary>
+        /// Number of items added
+        /// </summary>
+        private uint count { get; set; }
+
+        /// <summary>
+        /// Creates a new partitioned Bloom filter optimized to store n items with a
+        /// specified target false-positive rate.
+        /// </summary>
+        /// <param name="n">Number of items</param>
+        /// <param name="fpRate">Desired false-positive rate</param>
+        public PartitionedBloomFilter(uint n, double fpRate)
+        {
+            var m = ProbabilisticDataStructures.OptimalM(n, fpRate);
+            var k = ProbabilisticDataStructures.OptimalK(fpRate);
+            var partitions = new Buckets[k];
+            var s = (uint)Math.Ceiling((double)m / (double)k);
+
+            for (uint i = 0; i < k; i++)
+            {
+                partitions[i] = new Buckets(s, 1);
+            }
+
+            this.Partitions = partitions;
+            this.hash = HashAlgorithm.Create("MD5");
+            this.m = m;
+            this.k = k;
+            this.s = s;
+        }
+
+        /// <summary>
+        /// Returns the Bloom filter capacity, m.
+        /// </summary>
+        /// <returns>The Bloom filter capacity, m</returns>
+        public uint Capacity()
+        {
+            return this.m;
+        }
+
+        /// <summary>
+        /// Returns the number of hash functions.
+        /// </summary>
+        /// <returns>The number of hash functions</returns>
+        public uint K()
+        {
+            return this.k;
+        }
+
+        /// <summary>
+        /// Returns the number of items in the filter.
+        /// </summary>
+        /// <returns>The number of items in the filter</returns>
+        public uint Count()
+        {
+            return this.count;
+        }
+
+        /// <summary>
+        /// Returns the current estimated ratio of set bits.
+        /// </summary>
+        /// <returns>The current estimated ratio of set bits</returns>
+        public double EstimatedFillRatio()
+        {
+            return 1 - Math.Exp(-(double)this.count / (double)this.s);
+        }
+
+        /// <summary>
+        /// Returns the average ratio of set bits across all partitions.
+        /// </summary>
+        /// <returns>The average ratio of set bitsacross all partitions</returns>
+        public double FillRatio()
+        {
+            var t = (double)0;
+            for (uint i = 0; i < this.k; i++)
+            {
+                uint sum = 0;
+                for (uint j = 0; j < this.Partitions[i].Count; j++)
+                {
+                    sum += this.Partitions[i].Get(j);
+                }
+                t += ((double)sum / (double)this.s);
+            }
+            return (double)t / (double)this.k;
+        }
+
+        /// <summary>
+        /// Will test for membership of the data and returns true if it is a
+        /// member, false if not. This is a probabilistic test, meaning there is a
+        /// non-zero probability of false positives but a zero probability of false
+        /// negatives. Due to the way the filter is partitioned, the probability of
+        /// false positives is uniformly distributed across all elements.
+        /// </summary>
+        /// <param name="data">The data to test for</param>
+        /// <returns>Whether or not the data was found</returns>
+        public bool Test(byte[] data)
+        {
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+
+            // If any of the K partiion bits are not set, then it's not a member.
+            for (uint i = 0; i < this.k; i++)
+            {
+                if (this.Partitions[i].Get((lower + upper * i) % this.s) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Will add the data to the Bloom filter. It returns the filter to allow for
+        /// chaining.
+        /// </summary>
+        /// <param name="data">The data to add</param>
+        /// <returns>The PartitionedBloomFilter</returns>
+        public IFilter Add(byte[] data)
+        {
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+
+            // Set the K partition bits.
+            for (uint i = 0; i < this.k; i++)
+            {
+                this.Partitions[i].Set((lower + upper * i) % this.s, 1);
+            }
+
+            this.count++;
+            return this;
+        }
+
+        /// <summary>
+        /// Equivalent to calling Test followed by Add. It returns true if the data is a
+        /// member, false if not.
+        /// </summary>
+        /// <param name="data">The data to test for and add</param>
+        /// <returns>
+        /// Whether the data was present in the filter prior to adding it
+        /// </returns>
+        public bool TestAndAdd(byte[] data)
+        {
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+            var member = true;
+
+            // If any K partition bits are not set, then it's not a member.
+            for (uint i = 0; i < this.k; i++)
+            {
+                var idx = (lower + upper * i) % this.s;
+                if (this.Partitions[i].Get(idx) == 0)
+                {
+                    member = false;
+                }
+                this.Partitions[i].Set(idx, 1);
+            }
+
+            this.count++;
+            return member;
+        }
+
+        /// <summary>
+        /// Restores the Bloom filter to its original state. It returns the filter
+        /// to allow for chaining.
+        /// </summary>
+        /// <returns>The PartitionedBloomFilter</returns>
+        public PartitionedBloomFilter Reset()
+        {
+            foreach (var partition in this.Partitions)
+            {
+                partition.Reset();
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the hashing function used in the filter.
+        /// </summary>
+        /// <param name="h">The HashAlgorithm to use.</param>
+        // TODO: Add SetHash to the IFilter interface?
+        public void SetHash(HashAlgorithm h)
+        {
+            this.hash = h;
+        }
+    }
+}
---
+++ TestProbabilisticDataStructures/TestPartitionedBloomFilter.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProbabilisticDataStructures;
+using System.Text;
+
+namespace TestProbabilisticDataStructures
+{
+    [TestClass]
+    public class TestPartitionedBloomFilter
+    {
+        private static byte[] A_BYTES = Encoding.ASCII.GetBytes("a");
+        private static byte[] B_BYTES = Encoding.ASCII.GetBytes("b");
+        private static byte[] C_BYTES = Encoding.ASCII.GetBytes("c");
+        private static byte[] X_BYTES = Encoding.ASCII.GetBytes("x");
+
+        /// <summary>
+        /// Ensures that Capacity() returns the number of bits, m, in the Bloom filter.
+        /// </summary>
+        [TestMethod]
+        public void TestPartitionedCapacity()
+        {
+            var f = new PartitionedBloomFilter(100, 0.1);
+            var capacity = f.Capacity();
+
+            Assert.AreEqual(480u, capacity);
+        }
+
+        /// <summary>
+        /// Ensures that K() returns the number of hash functions in the Bloom Filter.
+        /// </summary>
+        [TestMethod]
+        public void TestPartitionedK()
+        {
+            var f = new PartitionedBloomFilter(100, 0.1);
+            var k = f.K();
+
+            Assert.AreEqual(4u, k);
+        }
+
+        /// <summary>
+        /// Ensures that Count returns the number of items added to the filter.
+        /// </summary>
+        [TestMethod]
+        public void TestPartitionedCount()
+        {
+            var f = new PartitionedBloomFilter(100, 0.1);
+            for (uint i = 0; i < 10; i++)
+            {
+                f.Add(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            var count = f.Count();
+            Assert.AreEqual(10u, count);
+        }
+
+        /// <summary>
+        /// Ensures that EstimatedFillRatio returns the correct approximation.
+        /// </summary>
+        [TestMethod]
+        public void TestPartitionedEstimatedFillRatio()
+        {
+            var f = new PartitionedBloomFilter(100, 0.5);
+            for (uint i = 0; i < 100; i++)
+            {
+                f.Add(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            var ratio = f.EstimatedFillRatio();
+            if (ratio > 0.5)
+            {
+                Assert.Fail("Expected less than or equal to 0.5, got {0}", ratio);
+            }
+        }
+
+        /// <summary>
+        /// Ensures that FillRatio returns the ratio of set bits.
+        /// </summary>
+        [TestMethod]
+        public void TestPartitionedFillRatio()
+        {
+            var f = new PartitionedBloomFilter(100, 0.1);
+            f.Add(A_BYTES);
+            f.Add(B_BYTES);
+            f.Add(C_BYTES);
+            f.Add(X_BYTES);
+
+            var ratio = f.FillRatio();
+            Assert.AreEqual(0.03125, ratio);
+        }
+
+        /// <summary>
+        /// Ensures that Test, Add, and TestAndAdd behave correctly.
+        /// </summary>
+        [TestMethod]
+        public void TestPartitionedBloomTestAndAdd()
+        {
+            var f = new PartitionedBloomFilter(100, 0.01);
+
+            // 'a' is not in the filter.
+            if (f.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should not be a member");
+            }
+
+            var addedF = f.Add(A_BYTES);
+            Assert.AreSame(f, addedF, "Returned PartitionedBloomFilter should be the same instance");
+
+            // 'a' is now in the filter.
+            if (!f.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'a' is still in the filter.
+            if (!f.TestAndAdd(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'b' is not in the filter.
+            if (f.TestAndAdd(B_BYTES))
+            {
+                Assert.Fail("'b' should not be a member");
+            }
+
+            // 'a' is still in the filter.
+            if (!f.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'b' is now in the filter.
+            if (!f.Test(B_BYTES))
+            {
+                Assert.Fail("'b' should be a member");
+            }
+
+            // 'c' is not in the filter.
+            if (f.Test(C_BYTES))
+            {
+                Assert.Fail("'c' should not be a member");
+            }
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                f.TestAndAdd(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            // 'x' should be a false positive.
+            if (!f.Test(X_BYTES))
+            {
+                Assert.Fail("'x' should be a member");
+            }
+        }
+
+        /// <summary>
+        /// Ensures that Reset sets every bit to zero.
+        /// </summary>
+        [TestMethod]
+        public void TestPartitionedBloomReset()
+        {
+            var f = new PartitionedBloomFilter(100, 0.1);
+            for (int i = 0; i < 1000; i++)
+            {
+                f.Add(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            var resetF = f.Reset();
+            Assert.AreSame(f, resetF, "Returned PartitionedBloomFilter should be the same instance");
+
+            foreach (var partition in f.Partitions)
+            {
+                for (uint i = 0; i < partition.Count; i++)
+                {
+                    if (partition.Get(0) != 0)
+                    {
+                        Assert.Fail("Expected all bits to be unset");
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkPartitionedBloomAdd()
+        {
+            var n = 100000;
+            var f = new PartitionedBloomFilter(100000, 0.1);
+            var data = new byte[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = Encoding.ASCII.GetBytes(i.ToString());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                f.Add(data[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkPartitionedBloomTest()
+        {
+            var n = 100000;
+            var f = new PartitionedBloomFilter(100000, 0.1);
+            var data = new byte[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = Encoding.ASCII.GetBytes(i.ToString());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                f.Test(data[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkPartitionedBloomTestAndAdd()
+        {
+            var n = 100000;
+            var f = new PartitionedBloomFilter(100000, 0.1);
+            var data = new byte[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = Encoding.ASCII.GetBytes(i.ToString());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                f.TestAndAdd(data[i]);
+            }
+        }
+    }
+}
