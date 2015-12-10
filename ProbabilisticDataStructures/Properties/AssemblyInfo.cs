fc427fc887eb5adc0ced9d162e296ad33669cd38
mattlorimor <mattlorimor@gmail.com>
2015-12-09 23:43:21 -0600

    Make all internal-scoped objects visible to the testing assembly.

--- ProbabilisticDataStructures/Properties/AssemblyInfo.cs
+++ ProbabilisticDataStructures/Properties/AssemblyInfo.cs
@@ -14,6 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Make all internals visible to the testing assembly.
+[assembly: InternalsVisibleTo("TestProbabilisticDataStructures")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
