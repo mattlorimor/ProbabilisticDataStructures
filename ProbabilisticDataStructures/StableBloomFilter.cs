5f72e4fe28390b24311cf37ba7899a64601f0d5a
mattlorimor <mattlorimor@gmail.com>
2015-12-11 22:54:02 -0600

    Implement StableBloomFilter.

---
+++ ProbabilisticDataStructures/StableBloomFilter.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbabilisticDataStructures
+{
+    /// <summary>
+    /// StableBloomFilter implements a Stable Bloom Filter as described by Deng and
+    /// Rafiei in Approximately Detecting Duplicates for Streaming Data using Stable
+    /// Bloom Filters:
+    ///
+    /// http://webdocs.cs.ualberta.ca/~drafiei/papers/DupDet06Sigmod.pdf
+    ///
+    /// A Stable Bloom Filter (SBF) continuously evicts stale information so that it
+    /// has room for more recent elements. Like traditional Bloom filters, an SBF
+    /// has a non-zero probability of false positives, which is controlled by
+    /// several parameters. Unlike the classic Bloom filter, an SBF has a tight
+    /// upper bound on the rate of false positives while introducing a non-zero rate
+    /// of false negatives. The false-positive rate of a classic Bloom filter
+    /// eventually reaches 1, after which all queries result in a false positive.
+    /// The stable-point property of an SBF means the false-positive rate
+    /// asymptotically approaches a configurable fixed constant. A classic Bloom
+    /// filter is actually a special case of SBF where the eviction rate is zero, so
+    /// this package provides support for them as well.
+    ///
+    /// Stable Bloom Filters are useful for cases where the size of the data set
+    /// isn't known a priori, which is a requirement for traditional Bloom filters,
+    /// and memory is bounded.  For example, an SBF can be used to deduplicate
+    /// events from an unbounded event stream with a specified upper bound on false
+    /// positives and minimal false negatives.
+    /// </summary>
+    public class StableBloomFilter : IFilter
+    {
+        /// <summary>
+        /// Filter data
+        /// </summary>
+        internal Buckets cells { get; set; }
+        /// <summary>
+        /// Hash algorightm
+        /// </summary>
+        private HashAlgorithm hash { get; set; }
+        /// <summary>
+        /// Number of cells
+        /// </summary>
+        internal uint m { get; set; }
+        /// <summary>
+        /// Number of cells to decrement
+        /// </summary>
+        private uint p { get; set; }
+        /// <summary>
+        /// Number of hash functions
+        /// </summary>
+        private uint k { get; set; }
+        /// <summary>
+        /// Cell max value
+        /// </summary>
+        internal byte max { get; set; }
+        /// <summary>
+        /// Buffer used to cache indices
+        /// </summary>
+        private uint[] indexBuffer { get; set; }
+
+        private Random random = new Random();
+
+        /// <summary>
+        /// Creates a new Stable Bloom Filter with m cells and d bits allocated per cell
+        /// optimized for the target false-positive rate. Use NewDefaultStableFilter if
+        /// you don't want to calculate d.
+        /// </summary>
+        /// <param name="m">Number of cells to decrement</param>
+        /// <param name="d">Bits per cell</param>
+        /// <param name="fpRate">Desired false-positive rate</param>
+        public static StableBloomFilter NewStableBloomFilter(uint m, byte d, double fpRate)
+        {
+            var k = ProbabilisticDataStructures.OptimalK(fpRate) / 2;
+            if (k > m)
+            {
+                k = m;
+            }
+            else if (k <= 0)
+            {
+                k = 1;
+            }
+
+            var cells = new Buckets(m, d);
+
+            return new StableBloomFilter
+            {
+                hash = HashAlgorithm.Create("MD5"),
+                m = m,
+                k = k,
+                p = optimalStableP(m, k, d, fpRate),
+                max = cells.MaxBucketValue(),
+                cells = cells,
+                indexBuffer = new uint[k]
+            };
+        }
+
+        /// <summary>
+        /// Creates a new Stable Bloom Filter with m 1-bit cells and which is optimized
+        /// for cases where there is no prior knowledge of the input data stream while
+        /// maintaining an upper bound using the provided rate of false positives.
+        /// </summary>
+        /// <param name="m">Number of cells to decrement</param>
+        /// <param name="fpRate">Desired false-positive rate</param>
+        public static StableBloomFilter NewDefaultStableBloomFilter(uint m, double fpRate)
+        {
+            return NewStableBloomFilter(m, 1, fpRate);
+        }
+
+        /// <summary>
+        /// Creates a new special case of Stable Bloom Filter which is a traditional
+        /// Bloom filter with m bits and an optimal number of hash functions for the
+        /// target false-positive rate. Unlike the stable variant, data is not evicted
+        /// and a cell contains a maximum of 1 hash value.
+        /// </summary>
+        /// <param name="m">Number of cells to decrement</param>
+        /// <param name="fpRate">Desired false-positive rate</param>
+        /// <returns></returns>
+        public static StableBloomFilter NewUnstableBloomFilter(uint m, double fpRate)
+        {
+            var cells = new Buckets(m, 1);
+            var k = ProbabilisticDataStructures.OptimalK(fpRate);
+
+            return new StableBloomFilter
+            {
+                hash = HashAlgorithm.Create("MD5"),
+                m = m,
+                k = k,
+                p = 0,
+                max = cells.MaxBucketValue(),
+                cells = cells,
+                indexBuffer = new uint[k]
+            };
+        }
+
+        /// <summary>
+        /// Returns the number of cells in the Stable Bloom Filter.
+        /// </summary>
+        /// <returns>The number of cells in the Stable Bloom Filter</returns>
+        public uint Cells()
+        {
+            return this.m;
+        }
+
+        /// <summary>
+        /// Returns the number of hash functions.
+        /// </summary>
+        /// <returns>The number of hash functions</returns>
+        public uint K()
+        {
+            return this.k;
+        }
+
+        /// <summary>
+        /// Returns the number of cells decremented on ever add.
+        /// </summary>
+        /// <returns></returns>
+        public uint P()
+        {
+            return this.p;
+        }
+
+        /// <summary>
+        /// Returns the limit of the expected fraction of zeros in the Stable Bloom
+        /// Filter when the number of iterations goes to infinity. When this limit is
+        /// reached, the Stable Bloom Filter is considered stable.
+        /// </summary>
+        /// <returns>
+        /// The limit of the expected fraction of zeros in the SBF as the number of
+        /// iterations approaches infinity.
+        /// </returns>
+        public double StablePoint()
+        {
+            var subDenom = this.p * (1.0 / (double)this.k - 1.0 / (double)this.m);
+            var denom = 1.0 + 1.0 / (double)subDenom;
+            var b = 1.0 / denom;
+
+            return Math.Pow(b, this.max);
+        }
+
+        /// <summary>
+        /// Returns the upper bound on false positives when the filter has become stable.
+        /// </summary>
+        /// <returns>
+        /// The upper bound on false positives when the filter has become stable
+        /// </returns>
+        public double FalsePositiveRate()
+        {
+            return Math.Pow(1 - this.StablePoint(), this.k);
+        }
+
+        /// <summary>
+        /// Will test for membership of the data and returns true if it is a member,
+        /// false if not. This is a probabilistic test, meaning there is a non-zero
+        /// probability of false positives but a zero probability of false negatives.
+        /// </summary>
+        /// <param name="data">The data to search for.</param>
+        /// <returns>Whether or not the data is maybe contained in the filter</returns>
+        public bool Test(byte[] data)
+        {
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+
+            // If any of the K cells are 0, then it's not a member.
+            for (uint i = 0; i < this.k; i++)
+            {
+                if (this.cells.Get((lower + upper * i) % this.m) == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Will add the data to the Bloom filter. It returns the filter to allow
+        /// for chaining.
+        /// </summary>
+        /// <param name="data">The data to add.</param>
+        /// <returns>The filter.</returns>
+        public IFilter Add(byte[] data)
+        {
+            // Randomly decrement p cells to make room for new elements.
+            this.decrement();
+
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+
+            // Set the K cells to max.
+            for (uint i = 0; i < this.k; i++)
+            {
+                this.cells.Set((lower + upper * i) % this.m, this.max);
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Equivalent to calling Test followed by Add. It returns true if the data is a
+        /// member, false if not.
+        /// </summary>
+        /// <param name="data">The data to test for and add.</param>
+        /// <returns>Whether or not the data was present before adding.</returns>
+        public bool TestAndAdd(byte[] data)
+        {
+            var hashKernel = ProbabilisticDataStructures.HashKernel(data, this.hash);
+            var lower = hashKernel.Item1;
+            var upper = hashKernel.Item2;
+            var member = true;
+
+            // If any of the K cells are 0, then it's not a member.
+            for (uint i = 0; i < this.k; i++)
+            {
+                this.indexBuffer[i] = (lower + upper * i) % this.m;
+                if (this.cells.Get(this.indexBuffer[i]) == 0)
+                {
+                    member = false;
+                }
+            }
+
+            // Randomly decrement p cells to make room for new elements.
+            this.decrement();
+
+            // Set the K cells to max.
+            foreach (var idx in this.indexBuffer)
+            {
+                this.cells.Set(idx, this.max);
+            }
+
+            return member;
+        }
+
+        /// <summary>
+        /// Restores the Stable Bloom Filter to its original state. It returns the filter to
+        /// allow for chaining.
+        /// </summary>
+        /// <returns>The reset bloom filter.</returns>
+        public StableBloomFilter Reset()
+        {
+            this.cells.Reset();
+            return this;
+        }
+
+        /// <summary>
+        /// Sets the hashing function used in the filter.
+        /// </summary>
+        /// <param name="h">The HashAlgorithm to use.</param>
+        // TODO: Add SetHash to the IFilter interface?
+        public void SetHash(HashAlgorithm h)
+        {
+            this.hash = h;
+        }
+
+        /// <summary>
+        /// Will decrement a random cell and (p-1) adjacent cells by 1. This is faster
+        /// than generating p random numbers. Although the processes of picking the p
+        /// cells are not independent, each cell has a probability of p/m for being
+        /// picked at each iteration, which means the properties still hold.
+        /// </summary>
+        private void decrement()
+        {
+            var r = random.Next((int)this.m);
+            for (uint i = 0; i < this.p; i++)
+            {
+                var idx = (r + i) % this.m;
+                this.cells.Increment((uint)idx, -1);
+            }
+        }
+
+        /// <summary>
+        /// Returns the optimal number of cells to decrement, p, per iteration for the
+        /// provided parameters of an SBF.
+        /// </summary>
+        /// <param name="m">Number of cells</param>
+        /// <param name="k">Number of hash functions</param>
+        /// <param name="d">Bits per cell</param>
+        /// <param name="fpRate">Desired false-positive rate</param>
+        /// <returns>Optimal number of cells to decrement</returns>
+        private static uint optimalStableP(uint m, uint k, byte d, double fpRate)
+        {
+            var max = Math.Pow(2, d) - 1;
+            var subDenom = Math.Pow(1 - Math.Pow(fpRate, 1.0 / k), 1.0 / max);
+            var denom = (1.0 / subDenom - 1) * (1.0 / k - 1.0 / m);
+
+            var p = 1.0 / denom;
+            if (p <= 0)
+            {
+                p = 1;
+            }
+
+            return (uint)p;
+        }
+    }
+}
---
+++ TestProbabilisticDataStructures/TestStableBloomFilter.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProbabilisticDataStructures;
+using System.Text;
+
+namespace TestProbabilisticDataStructures
+{
+    [TestClass]
+    public class TestStableBloomFilter
+    {
+        private static byte[] A_BYTES = Encoding.ASCII.GetBytes("a");
+        private static byte[] B_BYTES = Encoding.ASCII.GetBytes("b");
+        private static byte[] C_BYTES = Encoding.ASCII.GetBytes("c");
+        private static byte[] X_BYTES = Encoding.ASCII.GetBytes("x");
+
+        /// <summary>
+        /// Ensures that NewUnstableBloomFilter creates a Stable Bloom Filter with p=0,
+        /// max=1 and k hash functions.
+        /// </summary>
+        [TestMethod]
+        public void TestNewUnstableBloomFilter()
+        {
+            var f = StableBloomFilter.NewUnstableBloomFilter(100, 0.1);
+            var k = ProbabilisticDataStructures.ProbabilisticDataStructures.OptimalK(0.1);
+
+            Assert.AreEqual(k, f.K());
+            Assert.AreEqual(100u, f.m);
+            Assert.AreEqual(0u, f.P());
+            Assert.AreEqual(1u, f.max);
+        }
+
+        /// <summary>
+        /// Ensures that Cells returns the number of cells, m, in the Stable Bloom
+        /// Filter.
+        /// </summary>
+        [TestMethod]
+        public void TestStableCells()
+        {
+            var f = StableBloomFilter.NewStableBloomFilter(100, 1, 0.1);
+
+            Assert.AreEqual(100u, f.Cells());
+        }
+
+        /// <summary>
+        /// Ensures that K returns the number of hash functions in the Stable Bloom
+        /// Filter.
+        /// </summary>
+        [TestMethod]
+        public void TestStableK()
+        {
+            var f = StableBloomFilter.NewStableBloomFilter(100, 1, 0.01);
+
+            Assert.AreEqual(3u, f.K());
+        }
+
+        /// <summary>
+        /// Ensures that Test, Add, and TestAndAdd behave correctly.
+        /// </summary>
+        [TestMethod]
+        public void TestStableTestAndAdd()
+        {
+            var f = StableBloomFilter.NewDefaultStableBloomFilter(10000, 0.01);
+
+            // 'a' is not in the filter.
+            if (f.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should not be a member");
+            }
+
+            var addedF = f.Add(A_BYTES);
+            Assert.AreSame(f, addedF, "Returned StableBloomFilter should be the same instance");
+
+            // 'a' is now in the filter.
+            if (!f.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'a' is still in the filter.
+            if (!f.TestAndAdd(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'b' is not in the filter.
+            if (f.TestAndAdd(B_BYTES))
+            {
+                Assert.Fail("'b' should not be a member");
+            }
+
+            // 'a' is still in the filter.
+            if (!f.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should be a member");
+            }
+
+            // 'b' is now in the filter.
+            if (!f.Test(B_BYTES))
+            {
+                Assert.Fail("'b' should be a member");
+            }
+
+            // 'c' is not in the filter.
+            if (f.Test(C_BYTES))
+            {
+                Assert.Fail("'c' should not be a member");
+            }
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                f.TestAndAdd(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            // 'a' should have been evicted
+            if (f.Test(A_BYTES))
+            {
+                Assert.Fail("'a' should not be a member");
+            }
+        }
+
+        /// <summary>
+        /// Ensures that StablePoint returns the expected fraction of zeros for large
+        /// iterations.
+        /// </summary>
+        [TestMethod]
+        public void TestStablePoint()
+        {
+            var f = StableBloomFilter.NewDefaultStableBloomFilter(10000, 0.1);
+            for (int i = 0; i < 1000000; i++)
+            {
+                f.TestAndAdd(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            var zeros = 0;
+            for (uint i = 0; i < f.m; i++)
+            {
+                if (f.cells.Get(i) == 0)
+                {
+                    zeros++;
+                }
+            }
+
+            var actual = Math.Round((double)((double)zeros / (double)f.m), 1, MidpointRounding.AwayFromZero);
+            var expected = Math.Round(f.StablePoint(), 1, MidpointRounding.AwayFromZero);
+
+            Assert.AreEqual(expected, actual);
+
+            // A classic Bloom filter is a special case of SBF where P is 0 and max is 1.
+            // It doesn't have a stable point.
+            var bf = StableBloomFilter.NewUnstableBloomFilter(1000, 0.1);
+            var stablePoint = bf.StablePoint();
+            if (stablePoint != 0)
+            {
+                Assert.Fail(string.Format("Expected stable point 0, got {0}", stablePoint));
+            }
+        }
+
+        // Ensures that FalsePositiveRate returns the upper bound on false positives
+        // for stable filters.
+        [TestMethod]
+        public void TestStableFalsePositiveRate()
+        {
+            var f = StableBloomFilter.NewDefaultStableBloomFilter(1000, 0.01);
+            var fps = Math.Round(f.FalsePositiveRate(), 2, MidpointRounding.AwayFromZero);
+            Assert.IsFalse(fps > 0.01);
+
+            // Classic Bloom filters have an unbound rate of false positives. Once they
+            // become full, every query returns a false positive.
+            var bf = StableBloomFilter.NewUnstableBloomFilter(1000, 0.01);
+            fps = bf.FalsePositiveRate();
+            Assert.AreEqual(1.0, fps);
+        }
+
+        /// <summary>
+        /// Ensures that Reset sets every cell to zero
+        /// </summary>
+        [TestMethod]
+        public void TestStableReset()
+        {
+            var f = StableBloomFilter.NewDefaultStableBloomFilter(1000, 0.01);
+            for (int i = 0; i < 1000; i++)
+            {
+                f.TestAndAdd(Encoding.ASCII.GetBytes(i.ToString()));
+            }
+
+            var resetF = f.Reset();
+            Assert.AreSame(f, resetF, "Returned StableBloomFilter should be the same instance");
+
+            for (uint i = 0; i < f.m; i++)
+            {
+                var cell = f.cells.Get(i);
+                if (cell != 0)
+                {
+                    Assert.Fail(string.Format("Expected zero cell, got {0}", cell));
+                }
+            }
+        }
+    }
+
+    [TestClass]
+    public class BenchmarkStableBloomFilter
+    {
+        private StableBloomFilter f;
+        private uint n;
+        private byte[][] data;
+
+        [TestInitialize()]
+        public void Testinitialize()
+        {
+            n = 100000;
+            f = StableBloomFilter.NewDefaultStableBloomFilter(n, 0.01);
+            data = new byte[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = Encoding.ASCII.GetBytes(i.ToString());
+            }
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            f = null;
+            n = 0;
+            data = null;
+        }
+
+        [TestMethod]
+        public void BenchmarkStableAdd()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                f.Add(data[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkStableTest()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                f.Test(data[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkStableTestAndAdd()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                f.TestAndAdd(data[i]);
+            }
+        }
+    }
+
+    [TestClass]
+    public class BenchmarkUnstableBloomFilter
+    {
+        private StableBloomFilter f;
+        private uint n;
+        private byte[][] data;
+
+        [TestInitialize()]
+        public void Testinitialize()
+        {
+            n = 100000;
+            f = StableBloomFilter.NewUnstableBloomFilter(n, 0.1);
+            data = new byte[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = Encoding.ASCII.GetBytes(i.ToString());
+            }
+        }
+
+        [TestCleanup()]
+        public void TestCleanup()
+        {
+            f = null;
+            n = 0;
+            data = null;
+        }
+
+        [TestMethod]
+        public void BenchmarkUnstableAdd()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                f.Add(data[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkUnstableTest()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                f.Test(data[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BenchmarkUnstableTestAndAdd()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                f.TestAndAdd(data[i]);
+            }
+        }
+    }
+}
