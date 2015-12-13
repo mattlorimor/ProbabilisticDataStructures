689c29e8083e3dc19a26d2260e42f1f777eba319
mattlorimor <mattlorimor@gmail.com>
2015-12-12 18:42:21 -0600

    Implement TopK... sort of.

---
+++ ProbabilisticDataStructures/TopK.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C5;
+
+namespace ProbabilisticDataStructures
+{
+    /// <summary>
+    /// TopK uses a Count-Min Sketch to calculate the top-K frequent elements in a
+    /// stream.
+    /// </summary>
+    public class TopK
+    {
+        private CountMinSketch cms { get; set; }
+        private uint k { get; set; }
+        private uint n { get; set; }
+        //private IntervalHeap<Element> elements { get; set; }
+        private ElementHeap elements { get; set; }
+
+        /// <summary>
+        /// Creates a new TopK backed by a Count-Min sketch whose relative accuracy is
+        /// within a factor of epsilon with probability delta. It tracks the k-most
+        /// frequent elements.
+        /// </summary>
+        /// <param name="epsilon"></param>
+        /// <param name="delta"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static TopK NewTopK(double epsilon, double delta, uint k)
+        {
+            return new TopK
+            {
+                cms = new CountMinSketch(epsilon, delta),
+                k = k,
+                //elements = new IntervalHeap<Element>((int)k)
+                elements = new ElementHeap((int)k)
+            };
+        }
+
+        /// <summary>
+        /// Will add the data to the Count-Min Sketch and update the top-k heap if
+        /// applicable. Returns the TopK to allow for chaining.
+        /// </summary>
+        /// <param name="data">The data to add</param>
+        /// <returns>The TopK</returns>
+        public TopK Add(byte[] data)
+        {
+            this.cms.Add(data);
+            this.n++;
+
+            var freq = this.cms.Count(data);
+            if (this.isTop(freq))
+            {
+                this.insert(data, freq);
+            }
+
+            return this;
+        }
+
+        public Element[] Elements()
+        {
+            if (this.elements.Len() == 0)
+            {
+                return new Element[0];
+            }
+
+            return this.elements.elementHeap.OrderBy(x => x.Freq).ThenBy(x => x.added).ToArray();
+
+            //var elements = this.elements;
+            //var topK = new List<Element>((int)this.k);
+
+            //if (elements.Len() > 0)
+            //{
+            //    //return topK.OrderBy(x => x.Freq).ToArray();
+            //    //foreach (var element in elements.elementHeap)
+            //    //{
+            //    //    topK.Add(elements.Pop());
+            //    //}
+            //}
+
+            //return topK.ToArray();
+        }
+
+        public TopK Reset()
+        {
+            this.cms.Reset();
+            this.elements = new ElementHeap((int)k);
+            this.n = 0;
+            return this;
+        }
+
+        private bool isTop(UInt64 freq)
+        {
+            if (this.elements.Len() < this.k)
+            {
+                return true;
+            }
+
+            return freq >= this.Elements()[0].Freq;
+        }
+
+        private void insert(byte[] data, UInt64 freq)
+        {
+            var elements = this.Elements();
+            for (int i = 0; i < elements.Count(); i++)
+            {
+                var element = elements[i];
+                if (Enumerable.SequenceEqual(data, element.Data))
+                {
+                    // Element alread in top-k.
+                    elements[i].Freq = freq;
+                    this.elements.elementHeap = elements.ToList();
+                    return;
+                    this.elements.elementHeap[i].Freq = freq;
+                    return;
+                }
+            }
+
+            if (this.elements.Len() == this.k)
+            {
+                // Remove minimum-frequency element.
+                this.elements.Pop();
+            }
+
+            // Add element to top-k.
+            this.elements.Push(new Element
+            {
+                Data = data,
+                Freq = freq,
+                added = DateTime.UtcNow.Ticks
+            });
+        }
+
+        internal class ElementHeap
+        {
+            internal List<Element> elementHeap { get; set; }
+
+            internal int Len()
+            {
+                return this.elementHeap.Count;
+            }
+
+            internal bool Less(int i, int j)
+            {
+                return this.elementHeap[i].Freq < elementHeap[j].Freq;
+            }
+
+            internal void Swap(int i, int j)
+            {
+                var temp = this.elementHeap[i];
+                elementHeap[i] = elementHeap[j];
+                elementHeap[j] = temp;
+            }
+
+            internal void Push(Element x)
+            {
+                this.elementHeap.Add(x);
+            }
+
+            internal Element Pop()
+            {
+                var elementToRemove = this.elementHeap
+                    .OrderBy(x => x.Freq)
+                    .ThenBy(x => x.added)
+                    .First();
+                this.elementHeap.Remove(elementToRemove);
+                return elementToRemove;
+                //var n = this.elementHeap.Count - 1;
+                //var x = this.elementHeap[n];
+                //this.elementHeap = this.elementHeap.Take(n).ToList();
+                //return x;
+            }
+
+            internal ElementHeap(int k)
+            {
+                this.elementHeap = new List<Element>(k);
+            }
+        }
+
+        public class Element
+        {
+            public byte[] Data { get; set; }
+            public UInt64 Freq { get; set; }
+            internal long added { get; set; }
+        }
+    }
+}
---
+++ TestProbabilisticDataStructures/TestTopK.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProbabilisticDataStructures;
+using System.Text;
+using System.Linq;
+
+namespace TestProbabilisticDataStructures
+{
+    [TestClass]
+    public class TestTopK
+    {
+        private static byte[] BOB_BYTES = Encoding.ASCII.GetBytes("bob");
+        private static byte[] TYLER_BYTES = Encoding.ASCII.GetBytes("tyler");
+        private static byte[] FRED_BYTES = Encoding.ASCII.GetBytes("fred");
+        private static byte[] ALICE_BYTES = Encoding.ASCII.GetBytes("alice");
+        private static byte[] JAMES_BYTES = Encoding.ASCII.GetBytes("james");
+        private static byte[] SARA_BYTES = Encoding.ASCII.GetBytes("sara");
+        private static byte[] BILL_BYTES = Encoding.ASCII.GetBytes("bill");
+
+        [TestMethod]
+        public void TestTopk()
+        {
+            var topK = TopK.NewTopK(0.001, 0.99, 5);
+
+            topK.Add(BOB_BYTES).Add(BOB_BYTES).Add(BOB_BYTES);
+            // + BOB, BOB, BOB
+            topK.Add(TYLER_BYTES).Add(TYLER_BYTES).Add(TYLER_BYTES).Add(TYLER_BYTES).Add(TYLER_BYTES);
+            //   BOB, BOB, BOB
+            // + TYLER, TYLER, TYLER, TYLER, TYLER
+            topK.Add(FRED_BYTES);
+            //   BOB, BOB, BOB
+            //   TYLER, TYLER, TYLER, TYLER, TYLER
+            // + FRED
+            topK.Add(ALICE_BYTES).Add(ALICE_BYTES).Add(ALICE_BYTES).Add(ALICE_BYTES);
+            //   BOB, BOB, BOB
+            //   TYLER, TYLER, TYLER, TYLER, TYLER
+            //   FRED
+            // + ALICE, ALICE, ALICE, ALICE
+            topK.Add(JAMES_BYTES);
+            //   BOB, BOB, BOB
+            //   TYLER, TYLER, TYLER, TYLER, TYLER
+            //   FRED
+            //   ALICE, ALICE, ALICE, ALICE
+            // + JAMES
+            topK.Add(FRED_BYTES);
+            //   BOB, BOB, BOB
+            //   TYLER, TYLER, TYLER, TYLER, TYLER
+            // = FRED, FRED
+            //   ALICE, ALICE, ALICE, ALICE
+            //   JAMES
+            topK.Add(SARA_BYTES).Add(SARA_BYTES);
+            //   BOB, BOB, BOB
+            //   TYLER, TYLER, TYLER, TYLER, TYLER
+            //   FRED, FRED
+            //   ALICE, ALICE, ALICE, ALICE
+            // - JAMES
+            // + SARA, SARA
+
+            var addedK = topK.Add(BILL_BYTES);
+            //   BOB, BOB, BOB
+            //   TYLER, TYLER, TYLER, TYLER, TYLER
+            //   - FRED, FRED
+            //   ALICE, ALICE, ALICE, ALICE
+            //   SARA, SARA
+            //   BILL
+            Assert.AreSame(topK, addedK);
+            // latest one also
+            var expected = new ProbabilisticDataStructures.TopK.Element[]{
+                new ProbabilisticDataStructures.TopK.Element{Data=BILL_BYTES, Freq=1},
+                new ProbabilisticDataStructures.TopK.Element{Data=SARA_BYTES, Freq=2},
+                new ProbabilisticDataStructures.TopK.Element{Data=BOB_BYTES, Freq=3},
+                new ProbabilisticDataStructures.TopK.Element{Data=ALICE_BYTES, Freq=4},
+                new ProbabilisticDataStructures.TopK.Element{Data=TYLER_BYTES, Freq=5},
+            };
+
+            var actual = topK.Elements();
+
+            Assert.AreEqual(5, actual.Length);
+
+            for (int i = 0; i < actual.Length; i++)
+            {
+                var element = actual[i];
+                Assert.IsTrue(Enumerable.SequenceEqual(element.Data, expected[i].Data));
+                // freq check
+                Assert.AreEqual(expected[i].Freq, element.Freq);
+            }
+        }
+    }
+}
